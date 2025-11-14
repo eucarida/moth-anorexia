89d495d5327467c37a1d4453468e3f5263c8b197
eucarida <olle.heyman@outlook.com>
2025-11-14 11:05:25 +0100

    just an inisial cmit

---
+++ FoodWeighingMockUp_SCRIPT.R
@@ -0,0 +1,28 @@
+## This is a short script for determening the avrage weights of "Toppits Baking Cups Bakformar" and to find the weight to aim for when cuting the arva diet
+## Created by Eucarida 2025-11-14
+## Last dited by Eucarida 2025-11-14
+
+# clean work space
+rm(list = ls())
+gc()
+
+
+# load libraries (general)
+library(ggplot2)
+library(performance)
+library(GGally)
+library(gridExtra)
+library(grid)
+library(gtable)
+library(tidyverse)
+
+
+# read in data
+
+
+
+
+
+
+
+
